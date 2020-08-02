b262249bddb8843f36684f5199179050afc33135
akrinos <akrinos@vt.edu>
2020-08-01 21:07:49 -0400

    Generalizing this for sample data vs. any ENA?

--- submit_script/submit_snakemake.sh
+++ submit_script/submit_snakemake.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 snakemake   \
-        --jobs 100 --use-conda --profile tara-metag  \
+        --jobs 100 --use-conda -s EUKHeist  \
         --cluster-config cluster.yaml --cluster "sbatch --parsable --qos=unlim --partition={cluster.queue} --job-name=TARA.{rule}.{wildcards} --mem={cluster.mem}gb --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes}"
 
