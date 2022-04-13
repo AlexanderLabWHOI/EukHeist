e6c89a338db1a3e9d85a1dbe64ae0cf96928994e
akrinos <akrinos@vt.edu>
2022-04-13 11:32:26 -0400

    AIK's fixes to multiqc etc

--- submit_script/submit_snakemake.sh
+++ submit_script/submit_snakemake.sh
@@ -2,5 +2,5 @@
 
 snakemake   \
         --jobs 100 --use-conda -s EUKHeist  \
-        --cluster-config cluster.yaml --cluster "sbatch --parsable --qos=unlim --partition={cluster.queue} --job-name=TARA.{rule}.{wildcards} --mem={cluster.mem}gb --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes}"
+        --cluster-config cluster.yaml --cluster "sbatch --parsable --qos=unlim --partition={cluster.queue} --job-name=venteuks.{rule}.{wildcards} --mem={cluster.mem}gb --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes}"
 
